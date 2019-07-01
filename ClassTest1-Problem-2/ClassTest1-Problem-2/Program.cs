80f4bb0d98f901b0baa75200d28f91cac0c61312
Al Mustakim <badhonmdabdullah@gmail.com>
2019-07-02 02:00:28 +0600

    this is the initial commit

---
+++ ClassTest1-Problem-2/ClassTest1-Problem-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, i, flag = 0;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            int a = 0;
+            while (n != 0)
+            {
+                r = n % 10;
+                a = a * 10 + r;
+                n = n / 10;
+            }
+            //Checking Prime or not Prime 
+            for (i = 2; i <= a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.Write("Result is: " + a + "(Not Prime)");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.Write("Result is: " + a + "(Prime)");
+
+        }
+    }
+}
